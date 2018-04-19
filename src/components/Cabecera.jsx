477c2a83ad69e93fc20e52bf085c79df4aea3ff8
eric <favioticona@gmail.com>
2018-04-19 19:53:33 -0400

    primer commit

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,26 @@
+import React, { Component} from 'react'
+import {Menu, Header, Icon} from 'semantic-ui-react'
+class Cabecera extends Component {
+	render(){
+		return(
+			<div>
+				<Menu color="teal" inverted>
+					<Menu.Item header>Inicio</Menu.Item>
+					<Menu.Item active  position="right">Sube tu link</Menu.Item>
+					<Menu.Item position="right">Sube tu link</Menu.Item>
+				</Menu>				
+				<Header textAlign = "center">
+					<Icon name ="play" color="teal"/>
+			REACT LINKS
+					<Header.Subheader>
+			Bienvenido al sitio para encontrar recursos de React
+					</Header.Subheader>
+
+			Bienvenido al sitio
+				</Header>
+			</div>
+		)
+	}
+}
+
+export default Cabecera
---
+++ src/components/FormModal.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Modal, Input} from 'semantic-ui-react'
+
+export default class FormModal extends Component {
+	render(){
+		const {mostrar} = this.props
+
+		return(
+			<Modal open={mostrar}>
+				<Modal.Header>
+					Envianos tu link
+				</Modal.Header>
+				<Modal.Content>
+					Link <Input />
+				</Modal.Content>
+			</Modal>
+		)
+	}
+}
+
